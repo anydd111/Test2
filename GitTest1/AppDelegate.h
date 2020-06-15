152311e577ba1e40e6e6ae6d30ae33a0f7b46fd2
ZY <zhao123_321@163.com>
2020-06-15 21:36:17 +0800

    commit

---
+++ GitTest1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by user on 2020/6/15.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GitTest1/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  GitTest
+//
+//  Created by user on 2020/6/15.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
